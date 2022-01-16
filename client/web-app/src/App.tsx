1c2f4cb6ff145cb71095e4d87bda100ce00ce973
Armand Rozanski <armand.rozanski@gmail.com>
2022-01-16 23:07:42 +0000

    add material UI

--- client/web-app/src/App.tsx
+++ client/web-app/src/App.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { StyledWrapper } from './styles';
+import Slider from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+
+const CustomizedSlider = styled(Slider)`
+  color: #20b2aa;
+
+  :hover {
+    color: #2e8b57;
+  }
+`;
 
 function App() {
   return (
@@ -12,6 +22,7 @@ function App() {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
+          <CustomizedSlider defaultValue={20} />
           <a
             className="App-link"
             href="https://reactjs.org"
